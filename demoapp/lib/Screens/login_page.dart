4a1098a3aee8b8da99e10b4467b314f50374f884
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-09-05 05:36:27 -0700

    Added form validation

--- demoapp/lib/Screens/login_page.dart
+++ demoapp/lib/Screens/login_page.dart
@@ -9,6 +9,22 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  TextEditingController _userNameTEC = TextEditingController();
+  TextEditingController _userPassTEC = TextEditingController();
+
+  Loginbtn() {
+    if (_formkey.currentState!.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
+
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,110 +35,125 @@ class _LoginState extends State<Login> {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Container(
-          child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const SizedBox(
-                height: 150,
-              ),
-              const Text(
-                "LOGIN",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+          child: Form(
+            key: _formkey,
+            child: Center(
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const SizedBox(
+                  height: 150,
                 ),
-              ),
-
-              // Input Fields
-              // UserName
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.person),
-                    hintText: 'User Name',
-                    fillColor: Colors.grey[200],
-                    filled: true,
+                const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
-              ),
-
-              // Password
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.lock),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                  ),
-                ),
-              ),
-
-              // LoginBtn
-              Container(
-                margin: const EdgeInsets.all(20),
-                width: double.infinity,
-                child: ElevatedButton(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      print("Login");
-                    },
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
+          
+                // Input Fields
+                // UserName
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(top: 20),
+                  child: TextFormField(
+                    controller: _userNameTEC,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter username';
+                          }
+                          return null;
+                        },
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.transparent),
                       ),
-                    )),
-              ),
-
-              // SignUPbtn
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Does not have account? '),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Colors.blue,
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.blue),
                       ),
-                      onPressed: (() {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (builder) => const Signup(),
-                          ),
-                        );
-                      }),
-                      child: Text('Signup'),
-                    )
-                  ],
+                      prefixIcon: const Icon(Icons.person),
+                      hintText: 'User Name',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
                 ),
-              ),
-
-              
-            ]),
+          
+                // Password
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(top: 20),
+                  child: TextFormField(
+                    controller: _userPassTEC,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          }
+                          return null;
+                        },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      prefixIcon: const Icon(Icons.lock),
+                      hintText: 'Password',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+          
+                // LoginBtn
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: Loginbtn,
+                      style: TextButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                      )),
+                ),
+          
+                // SignUPbtn
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Does not have account? '),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.blue,
+                        ),
+                        onPressed: (() {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (builder) => const Signup(),
+                            ),
+                          );
+                        }),
+                        child: Text('Signup'),
+                      )
+                    ],
+                  ),
+                ),
+          
+                
+              ]),
+            ),
           ),
         ),
       ),
--- demoapp/lib/Screens/signup_page.dart
+++ demoapp/lib/Screens/signup_page.dart
@@ -10,175 +10,218 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
-  Signupbtn()
-  {
-    print("SignBTN");
-    // if()
-    // {
-    //   Toast.show("Please enter Email", duration: Toast.lengthLong, gravity:  Toast.bottom);
-    // }
+  TextEditingController _userNameTEC = TextEditingController();
+  TextEditingController _userEmailTEC = TextEditingController();
+  TextEditingController _userPassTEC = TextEditingController();
+  TextEditingController _userConPassTEC = TextEditingController();
+
+
+  Signupbtn() {
+    if (_formkey.currentState!.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
   }
+
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sign Up"),
       ),
-
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Container(
-          child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const SizedBox(
-                height: 150,
-              ),
-              const Text(
-                "Signup",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                ),
-              ),
-
-              // Input Fields
-              // UserEmail
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.person),
-                    hintText: 'User Email',
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                  ),
-                ),
-              ),
-
-              // UserName
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.person_outlined),
-                    hintText: 'User Name',
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                  ),
-                ),
-              ),
-
-              // Password
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.lock),
-                    hintText: 'Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                  ),
-                ),
-              ),
-
-              // Confirm Password
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(top: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    prefixIcon: const Icon(Icons.lock),
-                    hintText: 'Confirm Password',
-                    fillColor: Colors.grey[200],
-                    filled: true,
-                  ),
-                ),
-              ),
-
-              // SignupBtn
-              Container(
-                margin: const EdgeInsets.all(20),
-                width: double.infinity,
-                child: ElevatedButton(
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: Signupbtn,
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                      ),
-                    )),
-              ),
-
-              // SignUPbtn
-              Container(
-                child: Row(
+          child: Form(
+            key: _formkey,
+            child: Center(
+              child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Already a user? '),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Colors.blue,
+                    const SizedBox(
+                      height: 150,
+                    ),
+                    const Text(
+                      "Signup",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
                       ),
-                      onPressed: (() {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (builder) => const Login(),
-                          ),
-                        );
-                      }),
-                      child: Text('Login'),
-                    )
-                  ],
-                ),
-              ),
+                    ),
 
-              
-            ]),
+                    // Input Fields
+                    // UserEmail
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextFormField(
+                        controller: _userEmailTEC,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          prefixIcon: const Icon(Icons.person),
+                          hintText: 'User Email',
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+
+                    // UserName
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextFormField(
+                        controller: _userNameTEC,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter username';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          prefixIcon: const Icon(Icons.person_outlined),
+                          hintText: 'User Name',
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+
+                    // Password
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextFormField(
+                        controller: _userPassTEC,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          prefixIcon: const Icon(Icons.lock),
+                          hintText: 'Password',
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+
+                    // Confirm Password
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      margin: const EdgeInsets.only(top: 20),
+                      child: TextFormField(
+                        controller: _userConPassTEC,
+                        validator: (value) {
+                          if(value == null || value.isEmpty)
+                          {
+                            return 'Please enter password';
+                          }
+                          else if (_userPassTEC.text != _userConPassTEC.text) {
+                            return 'Does Not Match';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          prefixIcon: const Icon(Icons.lock),
+                          hintText: 'Confirm Password',
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                    ),
+
+                    // SignupBtn
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: Signupbtn,
+                          style: TextButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                            ),
+                          )),
+                    ),
+
+                    // SignUPbtn
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Already a user? '),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.blue,
+                            ),
+                            onPressed: (() {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (builder) => const Login(),
+                                ),
+                              );
+                            }),
+                            child: Text('Login'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ]),
+            ),
           ),
         ),
       ),
-
     );
   }
 }
